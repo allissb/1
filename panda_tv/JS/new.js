618636b4c9b79e207623dc89e3777830b60167cd
allissb <837808554@qq.com>
2019-05-04 16:41:14 +0800

    Add files via upload

--- panda_tv/JS/new.js
+++ panda_tv/JS/new.js
@@ -1,12 +1,200 @@
-﻿var i_fix=document.getElementsByClassName("breathe");
-var dotimer = uptimer = null;
-var speed=-2;
+﻿new Vue({
+	el:'#panda_header',
+	data:{
+		nav_first:['首页','全部','分类','熊猫精品','iPanda','熊猫游戏'],
+	}
+})
 
-var time=0;
-var i_stars_li=document.getElementsByClassName("stars-lists")[0].getElementsByClassName("stars-list");
-var i_stars_move=document.getElementsByClassName("index-pandastars")[0].getElementsByClassName("bw")[0].getElementsByClassName("stars")[0].getElementsByClassName("stars-lists")[0]
-var i_arrow=document.getElementsByClassName("index-pandastars")[0].getElementsByClassName("bw")[0].getElementsByClassName("stars")[0].getElementsByClassName("arrow");
+new Vue({
+	el:'#small',
+	data:{
+		nav_first:[
+		'img/wrap/right/29dc740285c75c514e2caadea13dfeba.webp',
+		'img/wrap/right/58fddbf3b377424b45ec928056a8b580.webp',
+		'img/wrap/right/d14363d8ae0eae10d462a1bfe34f7223.webp',
+		'img/wrap/right/51c33611e011c95d4438758c4bfe4d50.webp',
+		'img/wrap/right/270ac643d388b855719761f0bc165283.webp',
+		'img/wrap/right/beec09f880cc611ddbb889265066f472.webp'
+		],
+	}
+})
 
+new Vue({
+	el:'#sec',
+	data:{
+		jiji:[
+		'img/wrap/firgue-list/1.png',
+		'img/wrap/firgue-list/2.png',
+		'img/wrap/firgue-list/3.png',
+		'img/wrap/firgue-list/4.png'
+		],
+		zjdj:[
+		'img/wrap/firgue-list/5.png',
+		'img/wrap/firgue-list/6.png',
+		'img/wrap/firgue-list/7.png',
+		'img/wrap/firgue-list/8.png'
+		],
+		yllm:[
+		'img/wrap/firgue-list/9.png',
+		'img/wrap/firgue-list/10.png',
+		'img/wrap/firgue-list/11.png',
+		'img/wrap/firgue-list/12.png'
+		],
+	}
+})
+
+new Vue({
+	el:'#thr',
+	data:{
+		links:['[公告]保障自身权益，警惕黑产！','[活动]观风使志愿者团队第一期','[活动]手游网游专区幸运礼物上线'],
+		rank:[
+		{id:1,rank:'rnak-1',name:'鱼鱼666666',src:'img/wrap/2/rank/1.webp'},
+		{id:2,rank:'rnak-2',name:'叔叔别问我了我约',src:'img/wrap/2/rank/2.webp'},
+		{id:3,rank:'rnak-3',name:'天地佛爷',src:'img/wrap/2/rank/3.webp'},
+		{id:4,rank:'rnak-4',name:'逆风_舞姬',src:'img/wrap/2/rank/4.webp'},
+		{id:5,rank:'rnak-5',name:'天线giaogiao',src:'img/wrap/2/rank/5.webp'},
+		{id:6,rank:'rnak-6',name:'义乌拳头',src:'img/wrap/2/rank/6.webp'},
+		{id:7,rank:'rnak-7',name:'心情712956777',src:'img/wrap/2/rank/7.webp'},
+		{id:8,rank:'rnak-8',name:'iG_VK',src:'img/wrap/2/rank/8.webp'},
+		{id:9,rank:'rnak-9',name:'黑马精品万炮',src:'img/wrap/2/rank/9.webp'},
+		]
+	}
+})
+
+new Vue({
+	el:'#four',
+	data:{
+		list:[
+		{id:1,src:'img/wrap/3/1.webp',title:'熊猫正在慢慢的消逝 陪伴是最好的报答',name:'strong丶霸狼',num:'16702'},
+		{id:2,src:'img/wrap/3/2.webp',title:'【学姐】偶尔会想起我吗❤',name:'卿卿学姐',num:'9689'},
+		{id:3,src:'img/wrap/3/3.webp',title:'进来聊聊吧。最后一小时熊猫关闭了！',name:'熊猫丶乐鱼阿卡丽',num:'2175'},
+		{id:4,src:'img/wrap/3/4.webp',title:'跟你们说个事',name:'模叫兽',num:'4829'},
+		{id:5,src:'img/wrap/3/5.webp',title:'LPL春季赛 VG vs JDG',name:'LPL熊猫官方直播',num:'856311'},
+		{id:6,src:'img/wrap/3/6.webp',title:'感谢熊猫，感谢大家',name:'访谈嘉宾',num:'607'},
+		{id:7,src:'img/wrap/3/7.webp',title:'英雄联盟视频大赏',name:'英雄联盟活动台',num:'0'},
+		{id:8,src:'img/wrap/3/8.webp',title:'群19099991我不想再失去你们',name:'沈子涵',num:'209'},
+		]
+	}
+})
+
+new Vue({
+	el:'#five',
+	data:{
+		list:[
+		{id:1,src:'img/wrap/5/1.webp',title:'金曲歌王',name:'林俊杰',num:'0'},
+		{id:2,src:'img/wrap/5/2.webp',title:'国民女神',name:'Angelababy',num:'0'},
+		{id:3,src:'img/wrap/5/3.webp',title:'篮球巨星',name:'孙悦Qtip',num:'0'},
+		{id:4,src:'img/wrap/5/4.webp',title:'爱你哟仙仙',name:'baby半仙',num:'0'},
+		{id:5,src:'img/wrap/5/5.webp',title:'脏王之王',name:'王师傅',num:'0'},
+		{id:6,src:'img/wrap/5/6.webp',title:'IG战队中单',name:'IG.Rookie',num:'0'},
+		{id:7,src:'img/wrap/5/7.webp',title:'IG世界冠军上单',name:'Panda丶TheShy',num:'0'},
+		{id:8,src:'img/wrap/5/8.webp',title:'世界冠军上单',name:'iG_Duke',num:'0'},
+		{id:5,src:'img/wrap/5/9.webp',title:'金牌主持',name:'暄哥',num:'0'},
+		{id:5,src:'img/wrap/5/10.webp',title:'最强武器大师',name:'贾克虎丶虎神',num:'0'},
+		{id:5,src:'img/wrap/5/11.webp',title:'最美歌姬星妈',name:'Igxingsu',num:'0'},
+		{id:5,src:'img/wrap/5/12.webp',title:'心大集团',name:'苏C皮蛋',num:'0'},
+		{id:5,src:'img/wrap/5/13.webp',title:'鼠大王',name:'鼠大王',num:'0'},
+		{id:5,src:'img/wrap/5/14.webp',title:'南枪李锦',name:'李锦',num:'0'},
+		{id:5,src:'img/wrap/5/15.webp',title:'麦克李',name:'JamLee',num:'0'},
+		{id:5,src:'img/wrap/5/16.webp',title:'岛田老叉',name:'LaoX2333',num:'0'},
+		{id:5,src:'img/wrap/5/17.webp',title:'百变歌者',name:'Mars毒药',num:'0'}
+		],
+		active:[' transform:translateX(0px)','transform:translateX(-980px)','transform:translateX(-1400px)'],
+		current:0
+	},
+	methods:{
+		nex:function(){
+			this.current==2?this.current=2:this.current+=1;
+		},
+		pre:function(){
+			this.current==0?this.current=0:this.current-=1;
+		}
+	}
+})
+
+new Vue({
+	el:'#six',
+	data:{
+		list:[
+		{id:1,src:'img/wrap/6/0.webp',title:'剧本杀青衣怪谈biubiu',name:'饭岛ash'},
+		{id:2,src:'img/wrap/6/1.webp',title:'微博:周cici酱，qq群已满',name:'周cici酱'},
+		{id:3,src:'img/wrap/6/2.webp',title:'微博：mua车车酱',name:'mua车车酱'},
+		{id:4,src:'img/wrap/6/3.webp',title:'感谢曾一起走过的青春岁月！',name:'PD丶叫兽'},
+		{id:5,src:'img/wrap/6/4.webp',title:'关注微博：GRACEKKKKKK',name:'大姨妈阿'},
+		{id:6,src:'img/wrap/6/5.webp',title:'关注微博：皮阿卡9丶，感恩遇见。',name:'皮阿卡9丶'},
+		{id:7,src:'img/wrap/6/6.webp',title:'IG牛逼！',name:'李瑞轩'},
+		{id:8,src:'img/wrap/6/7.webp',title:'heeeey',name:'paipaipaipaipai'},
+		{id:9,src:'img/wrap/6/8.webp',title:'休息，有事私信',name:'小能同志'},
+		{id:10,src:'img/wrap/6/9.webp',title:'微博关注：沙拉小宝贝',name:'沙拉sala'},
+		{id:11,src:'img/wrap/6/10.webp',title:'微博关注：细软绫小萌',name:'细软绫小萌'},
+		{id:12,src:'img/wrap/6/11.webp',title:'微博尢娜酱 感恩遇见',name:'尢娜'},],
+		current:1,
+		width:-980,
+	},
+	computed:{
+		lef:function(){
+			return this.current*this.width;
+		}
+	},
+	created:function(){
+		var leng=this.list.length;
+		var pr=[];
+		var ne=[]
+		for(var i=leng;i>0;i--){
+			if(i>leng-4){
+				pr.unshift(this.list[i-1])
+			}
+		}
+		for(var i=0;i<leng;i++){
+			if(i<4){
+				ne.push(this.list[i])
+			}
+		}
+		this.list=this.list.concat(ne);
+		this.list=pr.concat(this.list);
+	},
+	methods:{
+		pre:function(){
+			if(this.current!=0)this.current-=1;
+		},
+		nex:function(){
+			if(this.current!=4)this.current+=1;
+		}
+	}
+})
+
+new Vue({
+	el:'#lol',
+	data:{
+		labers:['虎神','张嘉文','小师弟','君克','Rookie','芒果鱼','老刘','天琪'],
+		list:[
+		{id:1,src:'img/wrap/bw/1/1.webp',name:'一情书一',title:'爱过。致熊猫',num:'7359'},
+		{id:2,src:'img/wrap/bw/1/2.webp',name:'火苗OB解说',title:'世界第一亚索',num:'4759'},
+		{id:3,src:'img/wrap/bw/1/3.webp',name:'熊猫TV丶游戏',title:'王者AD，接包天，上分',num:'2319'},
+		{id:4,src:'img/wrap/bw/1/4.webp',name:'LPL熊猫官方直播',title:'LPL春季赛 WE VS BLG',num:'1165166'},
+		{id:5,src:'img/wrap/bw/1/5.webp',name:'我真的没有钱畫訫',title:'等我叫我爸投资20亿进来拯救你',num:'29624'},
+		{id:6,src:'img/wrap/bw/1/6.webp',name:'hiddenface',title:'岁月极美，在于它的必然流逝',num:'21261'},
+		{id:7,src:'img/wrap/bw/1/7.webp',name:'一个很C的稻草人',title:'稻草人:438018372+这个群',num:'4329'},
+		{id:8,src:'img/wrap/bw/1/8.webp',name:'国宝难民平欧巴丶',title:'站好最后一班岗',num:'3522'},
+		]
+	}
+})
+
+new Vue({
+	el:"#footer",
+	data:{
+		title:[{ti:'weixin',info:{one:'官方微信',two:'扫一扫关注官方微信'}},{ti:'weibo',info:{one:'官方微博',two:'扫一扫关注官方微博'}},{ti:'app',info:{one:'扫一扫下载App',two:'立即下载App'}}],
+		list_one:['App下载','IOS户外直播','直播助手使用教程','Xsplit教程软件'],
+		list_two:['用户行为准则','不良信息投诉举报','版权声明及投诉流程','内容管理及处置条例']
+	}
+})
+
+new Vue({
+	el:'#lift',
+	data:{
+		title:[{ti:'anchor-btn',info:{one:'我要',two:'开播'}},{ti:'help-btn',info:{one:'帮助',two:'中心'}},{ti:'code-btn',info:{one:'扫码',two:'直播'}},{ti:'service-btn',info:{one:'客服',two:'支持'}}],
+	}
+})
 
 function checkWebp() {
     try{
@@ -30,71 +218,4 @@ change_img()
 
 var big_sr=document.getElementsByClassName("index-slider-video-container")[0].style.background;
 document.getElementsByClassName("index-slider-video-container")[0].style.background=big_sr.replace("webp","web.png");
-//stars两侧的箭头
-i_arrow[0].onclick=function(){
-	if(i_stars_move.style.transform=="translateX(-980px)"){
-		i_stars_move.style.transform="translateX(0px)";
-	}else if(i_stars_move.style.transform=="translateX(-1400px)"){
-		i_stars_move.style.transform="translateX(-980px)";
-	}
-}
-i_arrow[1].onclick=function(){
-	if(i_stars_move.style.transform=="translateX(0px)"){
-		i_stars_move.style.transform="translateX(-980px)";
-	}else if(i_stars_move.style.transform=="translateX(-980px)"){
-		i_stars_move.style.transform="translateX(-1400px)";
-	}
-}
-
-var li_af=$(".index-shuffer .stars .bx-viewport ul:eq(0) li:gt(7)");
-var li_be=$(".index-shuffer .stars .bx-viewport ul:eq(0) li:lt(4)");
-var a=$(".index-shuffer .stars .bx-viewport ul:eq(0)");
-a.prepend(li_af.clone(true));
-a.append(li_be.clone(true));
-
-var b=$(".index-shuffer .arrows a")
-
-
-b.eq(1).click(debounce(bb,400));
-b.eq(0).click(debounce(aa,400));
-
-function debounce(fn, wait) {
-var timer = null;            
-    return function() { 
-    	fn()
-    	var _this=this;
-    	$(this).off("click");
-        if (!timer) {
-        	timer = setTimeout(function() {
-        		$(_this).click(debounce(fn,wait));
-                timer = null;                    
-            }, wait);                
-        }            
-    }
-}
-
-function aa(){
-	var ul_tra=parseInt(a.css("left").slice(0,-2))+980;
-	a.css("left",ul_tra+"px")
-	a.css("transition-duration","0.4s")
-	if(a.css("left")=="-980px"){
-		setTimeout(function(){
-			a.css("transition-duration","0")
-			a.css("left","-2940px")
-			},400)
-	}
-}
-function bb(){
-	var ul_tra=parseInt(a.css("left").slice(0,-2))-980;
-	a.css("left",ul_tra+"px");
-	a.css("transition-duration","0.4s")
-	if(a.css("left")=="-2940px"){
-		setTimeout(function(){
-			a.css("transition-duration","0")
-			a.css("left","-980px")
-		},400)
-	}
-}
-
-
 
